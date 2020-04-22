e4fe822572830ec8b29c5ee62152a054034192b3
gwcolonials <37127157+gwcolonials@users.noreply.github.com>
2020-04-22 16:27:40 -0400

    4/21

--- js/index.js
+++ js/index.js
@@ -46,4 +46,20 @@
     backSpeed: 40,
     loop: true
   });
+
+  $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".flight h3").each(function() {
+        /* Check the location of each desired element */
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+        /* If the element is completely within bounds of the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+          if ($(this).css("opacity")==0) {$(this).fadeTo(3000,1);}
+        } else { //object goes out of view (scrolling up)
+          if ($(this).css("opacity")==1) {$(this).fadeTo(3000,0);}
+        }
+      });
+    }).scroll(); //invoke scroll-handler on page-load
+
 })(jQuery); // End of use strict
